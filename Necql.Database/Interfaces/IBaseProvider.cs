a3f7a518cdee892dba64d18d9dbb830f87ef8c99
Alexandre de Souza <alexandre@aledsz.com.br>
2020-08-20 20:17:12 -0300

    Add interface for Database provider

---
+++ Necql.Database/Interfaces/IBaseProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace Necql.Database.Interfaces {
+  /// <summary>
+  /// Interface for database provider
+  /// </summary>
+  public interface IBaseProvider {
+    public (IDbConnection, string) Connect ();
+    public void Disconnect ();
+    public bool IsConnected ();
+    public void Configure (DatabaseOptions options);
+    public string GetConnectionString ();
+    public (IDataReader, string) GetData (string query);
+    public (dynamic, string) Execute (string query);
+    public dynamic GetLastId ();
+  }
+}

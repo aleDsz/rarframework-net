accbf4a5c06ff8406d54cc12d9c0f15b36e1c6a8
Alexandre de Souza <alexandre@aledsz.com.br>
2020-08-20 20:12:04 -0300

    Add Field attribute

---
+++ Necql/Schemas/Field.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Necql.Enumerators;
+
+namespace Necql.Schemas {
+  /// <summary>
+  /// Field definition for Schema
+  /// </summary>
+  [AttributeUsage (AttributeTargets.All, AllowMultiple = true)]
+  public class Field : Attribute {
+    /// <summary>
+    /// Field name
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Field type
+    /// </summary>
+    public FieldTypes Type { get; set; }
+
+    /// <summary>
+    /// Field default value
+    /// </summary>
+    public dynamic Default { get; set; }
+
+    /// <summary>
+    /// Field can be null?
+    /// </summary>
+    public bool Null { get; set; }
+
+    /// <summary>
+    /// Field database name
+    /// </summary>
+    public string Source { get; set; }
+  }
+}

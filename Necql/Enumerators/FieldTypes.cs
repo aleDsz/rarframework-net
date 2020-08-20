f217cbc279a76f10e7bca3326a1149c01b2d59cb
Alexandre de Souza <alexandre@aledsz.com.br>
2020-08-20 20:12:39 -0300

    Add Field types enumerator

---
+++ Necql/Enumerators/FieldTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Necql.Enumerators {
+  /// <summary>
+  /// Field Types
+  /// </summary>
+  public enum FieldTypes {
+    String = 0,
+    Integer,
+    Json,
+    Boolean,
+    Date,
+    DateTime
+  }
+}

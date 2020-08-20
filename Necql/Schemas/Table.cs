4f71a23351b22f7cb6c4fcbb9b2a0a051e3c08ce
Alexandre de Souza <alexandre@aledsz.com.br>
2020-08-20 20:12:18 -0300

    Add Table attribute

---
+++ Necql/Schemas/Table.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Necql.Schemas {
+  /// <summary>
+  /// Schema definition for database table
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Class)]
+  public class Table : Attribute {
+    /// <summary>
+    /// Table/Collection's name
+    /// </summary>
+    public string Name { get; set; }
+  }
+}

bf433f3abd3b9937d1a729841afd11e81792ec9b
Alexandre de Souza <alexandre@aledsz.com.br>
2020-08-20 20:18:25 -0300

    Add entity for Database Options

---
+++ Necql.Database/DatabaseOptions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Necql.Database.Interfaces;
+
+namespace Necql.Database {
+  /// <summary>
+  /// Database options to connect
+  /// </summary>
+  public class DatabaseOptions {
+    /// <summary>
+    /// Database unique identifier
+    /// </summary>
+    public string Identifier { get; set; }
+
+    /// <summary>
+    /// Database name
+    /// </summary>
+    public string DatabaseName { get; set; }
+
+    /// <summary>
+    /// Username to access database
+    /// </summary>
+    public string Username { get; set; }
+
+    /// <summary>
+    /// Password to access database
+    /// </summary>
+    public string Password { get; set; }
+
+    /// <summary>
+    /// Database port
+    /// </summary>
+    public int Port { get; set; }
+
+    /// <summary>
+    /// Database host
+    /// </summary>
+    public string Host { get; set; }
+
+    /// <summary>
+    /// Database protocol
+    /// </summary>
+    public string Protocol { get; set; }
+
+    /// <summary>
+    /// Database provider
+    /// </summary>
+    public IBaseProvider Provider { get; set; }
+  }
+}

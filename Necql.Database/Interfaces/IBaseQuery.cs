22028114c5be79b767f3eb27d1d2f81d23929c64
Alexandre de Souza <alexandre@aledsz.com.br>
2020-08-20 20:18:14 -0300

    Add interface for Base Query inside Provider

---
+++ Necql.Database/Interfaces/IBaseQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+using Necql.Database.Models;
+
+namespace Necql.Database.Interfaces {
+  /// <summary>
+  /// Interface for query
+  /// </summary>
+  public interface IBaseQuery {
+    public dynamic Prepare (IDbConnection connection, Query query, Dictionary<string, dynamic> opts = null);
+    public dynamic Execute (IDbConnection connection, Query query, Dictionary<string, dynamic> opts = null);
+  }
+}

eafbc6de9ee9a33ff25896700d0d4866b1bc0cd4
Alexandre de Souza <alexandre@aledsz.com.br>
2020-08-20 20:17:45 -0300

    Add Query structure for providers

---
+++ Necql.Database/Models/Query.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Necql.Database.Models {
+  /// <summary>
+  /// Query definition to execute statements
+  /// </summary>
+  public class Query {
+    /// <summary>
+    /// A dictionary with all from tables using alias and Schema
+    /// </summary>
+    public Dictionary<string, dynamic> From { get; set; }
+
+    /// <summary>
+    /// Fields used to generate statement
+    /// </summary>
+    public List<string> Fields { get; set; }
+
+    /// <summary>
+    /// Parameters to prepare statement to execute query
+    /// </summary>
+    public dynamic Parameters { get; set; }
+  }
+}

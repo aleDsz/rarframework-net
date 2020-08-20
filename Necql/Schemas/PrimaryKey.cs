24fb5b9286a0f33ae0daba846c3d0111b5e53f6b
Alexandre de Souza <alexandre@aledsz.com.br>
2020-08-20 20:12:11 -0300

    Add Primary Key Field attribute

---
+++ Necql/Schemas/PrimaryKey.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Necql.Enumerators;
+
+namespace Necql.Schemas {
+  /// <summary>
+  /// Primary key definition for Schema
+  /// </summary>
+  [AttributeUsage (AttributeTargets.Class, AllowMultiple = true)]
+  public class PrimaryKey : Attribute {
+    /// <summary>
+    /// Primary key field
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Primary key type
+    /// </summary>
+    public PrimaryKeys Type { get; set; }
+
+    /// <summary>
+    /// Should auto generate value?
+    /// </summary>
+    public bool AutoGenerate { get; set; }
+  }
+}

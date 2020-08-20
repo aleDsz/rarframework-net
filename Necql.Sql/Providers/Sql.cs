4365b482fc5166d8f5218e20f270d40e3406a0d9
Alexandre de Souza <alexandre@aledsz.com.br>
2020-08-20 20:19:34 -0300

    Add Necql.Sql project to handle all SQL Providers

---
+++ Necql.Sql/Providers/Sql.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+
+using Necql.Database;
+using Necql.Database.Interfaces;
+
+namespace Necql.Sql.Providers {
+  public class Sql : IBaseProvider {
+    public Sql () { }
+
+    public void Configure (DatabaseOptions options) {
+      throw new NotImplementedException ();
+    }
+
+    public (IDbConnection, string) Connect () {
+      throw new NotImplementedException ();
+    }
+
+    public void Disconnect () {
+      throw new NotImplementedException ();
+    }
+
+    public (dynamic, string) Execute (string query) {
+      throw new NotImplementedException ();
+    }
+
+    public string GetConnectionString () {
+      throw new NotImplementedException ();
+    }
+
+    public (IDataReader, string) GetData (string query) {
+      throw new NotImplementedException ();
+    }
+
+    public dynamic GetLastId () {
+      throw new NotImplementedException ();
+    }
+
+    public bool IsConnected () {
+      throw new NotImplementedException ();
+    }
+  }
+}
